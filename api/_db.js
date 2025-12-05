43ab30d3b3321eed1abf0d04d9fe10274c82288a
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-12-05 15:08:23 +0300

    Create _db.js

---
+++ api/_db.js
@@ -0,0 +1,32 @@
+// api/_db.js
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
+});
+
+async function query(text, params) {
+  const res = await pool.query(text, params);
+  return res;
+}
+
+async function withTransaction(callback) {
+  const client = await pool.connect();
+  try {
+    await client.query('BEGIN');
+    const result = await callback(client);
+    await client.query('COMMIT');
+    return result;
+  } catch (e) {
+    await client.query('ROLLBACK');
+    throw e;
+  } finally {
+    client.release();
+  }
+}
+
+module.exports = {
+  query,
+  withTransaction
+};
