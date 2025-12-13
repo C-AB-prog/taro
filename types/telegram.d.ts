d48a754290fae979808fbd134b9cef26985df6ab
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-12-13 03:40:38 +0300

    Update telegram.d.ts

--- types/telegram.d.ts
+++ types/telegram.d.ts
@@ -8,6 +8,11 @@ declare global {
         initDataUnsafe?: any;
         ready: () => void;
         expand?: () => void;
+        HapticFeedback?: {
+          impactOccurred: (style: "light" | "medium" | "heavy" | "rigid" | "soft") => void;
+          notificationOccurred: (type: "error" | "success" | "warning") => void;
+          selectionChanged: () => void;
+        };
       };
     };
   }
