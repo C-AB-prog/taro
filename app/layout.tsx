d024287fda2a1216f444b8da403c9e25dda766d9
C-AB-prog <dima.d.chat.gpt@gmail.com>
2025-12-13 03:19:19 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,20 @@
 import "./globals.css";
+import Script from "next/script";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Карта Дня | Daily Tarot"
+  title: "Карта Дня | Daily Tarot",
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ru">
+      <head>
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
